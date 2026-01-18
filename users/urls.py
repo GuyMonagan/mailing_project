dd2672944d6f7fd0868d895ce02604008d05875b
GuyMonagan <werevalkury@gmail.com>
2026-01-18 17:21:37 +0400

    создание профиля

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, ProfileView, ProfileEditView
 
 app_name = "users"
 
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/edit/", ProfileEditView.as_view(), name="profile_edit"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,11 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth import login
 from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, UpdateView
+from django.urls import reverse_lazy
+from .models import User
+
 
 class RegisterView(CreateView):
     form_class = CustomUserCreationForm
@@ -12,3 +17,17 @@ class RegisterView(CreateView):
         response = super().form_valid(form)
         login(self.request, self.object)  # логиним нового пользователя сразу
         return response
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "users/profile.html"
+
+
+class ProfileEditView(LoginRequiredMixin, UpdateView):
+    model = User
+    fields = ['avatar', 'phone', 'country']
+    template_name = "users/profile_edit.html"
+    success_url = reverse_lazy("users:profile")
+
+    def get_object(self):
+        return self.request.user
